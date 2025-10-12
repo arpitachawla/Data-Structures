3e9b8db0d325422691797b44d5636b4230f5e266
Arpita Chawla <arpitachawlakac@gmail.com>
2025-10-12 19:16:28 +0530

    Add files via upload
    Linked list operations like insertion, printing and all.

---
+++ linklist.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+//Creating a singly linked list and even transversing it insertion at end, beg
+typedef struct node1 {
+    int data;
+    struct node1 *link;
+} node;
+node* head;
+node* newnode();
+void insertEnd(int x);
+void insertBeg(int x);
+void print();
+int main() {
+    head= NULL;
+    int x,n;
+    scanf("%d", &n); // Taking the value of n
+    for(int i=0; i<n; i++){
+        printf("Enter the data to enter:\n");
+        scanf("%d", &x);
+        insertBeg(x);
+    }
+    print();
+}
+node* newnode(){
+    node *temp= (node*) malloc(sizeof(node));
+    return temp;
+}
+void insertEnd(int x){
+    node *temp= newnode();
+    temp->data=x;
+    temp->link=NULL;
+    if(head == NULL) head= temp;
+    else{
+        node* ptr= head;
+        while(ptr->link!= NULL){
+            ptr=ptr->link;
+        }
+        ptr->link=temp;
+    }
+}
+void print(){
+    node * temp1= head;
+    while(temp1!= NULL){
+        printf("%d\n", temp1->data);
+        temp1= temp1->link;
+    }
+}
+void insertBeg(int x){
+    node * temp= newnode();
+    temp->data=x;
+    temp->link=head;
+    head= temp;
+}
+//even make function for insert at any position
